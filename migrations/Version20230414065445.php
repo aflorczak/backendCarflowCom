6c421a6f992fc1b7e6ed960d9d0ccdc862d1fae9
Adrian Florczak <af_site@outlook.com>
2023-04-14 12:12:17 +0200

    added new fields for all modules (migrations)

---
+++ migrations/Version20230414065445.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230414065445 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE car ADD mileage INT NOT NULL, ADD number_of_seats INT NOT NULL, ADD number_of_doors INT NOT NULL, ADD fuel VARCHAR(50) NOT NULL, ADD body_type VARCHAR(50) NOT NULL, ADD segment VARCHAR(50) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE car DROP mileage, DROP number_of_seats, DROP number_of_doors, DROP fuel, DROP body_type, DROP segment');
+    }
+}
